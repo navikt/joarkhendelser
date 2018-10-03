10d1e9c1a0b36efde5c61e325fc2b584a7848bbf
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2018-10-03 19:23:22 +0200

    MMA-626: Sonar cleanup

--- app/src/main/java/no/nav/joarkinngaaendehendelser/consumer/kafka/JoarkSchema.java
+++ app/src/main/java/no/nav/joarkinngaaendehendelser/consumer/kafka/JoarkSchema.java
@@ -4,12 +4,11 @@ package no.nav.joarkinngaaendehendelser.consumer.kafka;
  * @author Martin Burheim Tingstad, Visma Consulting.
  */
 public class JoarkSchema {
-    public static String JOURNALPOST_ID = "JOURNALPOST_ID";
-    public static String JOURNALPOST = "T_JOURNALPOST";
-    public static String K_FAGOMRADE = "K_FAGOMRADE";
-    public static String K_JOURNALPOST_T = "K_JOURNALPOST_T";
-    public static String K_JOURNAL_S = "K_JOURNAL_S";
-    public static String K_MOTTAKS_KANAL = "K_MOTTAKS_KANAL";
-    public static String KANAL_REFERANSE_ID = "KANAL_REFERANSE_ID";
-
+    static final String JOURNALPOST_ID = "JOURNALPOST_ID";
+    static final String JOURNALPOST = "T_JOURNALPOST";
+    static final String K_FAGOMRADE = "K_FAGOMRADE";
+    static final String K_JOURNALPOST_T = "K_JOURNALPOST_T";
+    static final String K_JOURNAL_S = "K_JOURNAL_S";
+    static final String K_MOTTAKS_KANAL = "K_MOTTAKS_KANAL";
+    static final String KANAL_REFERANSE_ID = "KANAL_REFERANSE_ID";
 }
