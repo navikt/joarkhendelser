2720b9a408f68579db63ce29263fccefd57665e6
H150877 <bjornar.hunshamar@nav.no>
2018-10-04 12:22:28 +0200

    MMA-784 Skrevet tester for InngaaendeHendelsePublisher.

--- app/src/test/java/no/nav/joarkinngaaendehendelser/producer/InngaaendeHendelsePublisherTest.java
+++ app/src/test/java/no/nav/joarkinngaaendehendelser/producer/InngaaendeHendelsePublisherTest.java
@@ -1,25 +1,33 @@
 package no.nav.joarkinngaaendehendelser.producer;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-
-import static no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelsesType.MIDLERTIDIG_JOURNALFORT;
-import static org.junit.Assert.*;
-
 import no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostEndretEvent;
+import org.junit.After;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import static no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelsesType.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class InngaaendeHendelsePublisherTest {
 
-    JournalpostEndretEvent journalpostEndretEvent;
-    @Before
-    public void setUp() throws Exception {
-        journalpostEndretEvent = JournalpostEndretEvent.builder()
-                .journalpostStatusAfter("MO")
-                .journalpostStatusBefore("MO")
-                .operation("I")
-                .journalposttype("I")
+
+    @Mock
+    private KafkaTemplate<Object, Object> kafkaTemplate;
+
+    @InjectMocks
+    private InngaaendeHendelsePublisher publisher = new InngaaendeHendelsePublisher();
+
+    private JournalpostEndretEvent createJournalpostEndretEvent(String fagomradeBefore, String fagomradeAfter, String journalpostStatusBefore, String journalpostStatusAfter, String operation, String journalposttype) {
+        return JournalpostEndretEvent.builder()
+                .fagomradeBefore(fagomradeBefore)
+                .fagomradeAfter(fagomradeAfter)
+                .journalpostStatusAfter(journalpostStatusAfter)
+                .journalpostStatusBefore(journalpostStatusBefore)
+                .operation(operation)
+                .journalposttype(journalposttype)
                 .build();
     }
 
@@ -28,40 +36,37 @@ public class InngaaendeHendelsePublisherTest {
     }
 
     @Test
-    @Ignore
-    public void map() throws Exception {
-        InngaaendeHendelse map = new InngaaendeHendelsePublisher().map(journalpostEndretEvent);
+    public void shouldFinnMidlertidigJournalførtHendelsesType() {
+        JournalpostEndretEvent journalpostEndretEvent = createJournalpostEndretEvent("DAG", "FOR", "MO", "MO", "I", "I");
+        InngaaendeHendelse map = publisher.map(journalpostEndretEvent);
         assertEquals(MIDLERTIDIG_JOURNALFORT, map.getHendelsesType());
     }
 
     @Test
-    @Ignore
-    public void shouldFinnMidlertidigJournalførtHendelsesType() {
-        // TODO
-    }
-
-    @Test
-    @Ignore
     public void shouldFinnTemaEndretHendelsesType() {
-        // TODO
+        JournalpostEndretEvent journalpostEndretEvent = createJournalpostEndretEvent("DAG", "FOR", "MO", "J", "U", "I");
+        InngaaendeHendelse map = publisher.map(journalpostEndretEvent);
+        assertEquals(TEMA_ENDRET, map.getHendelsesType());
     }
 
     @Test
-    @Ignore
     public void shouldFinnEndeligJournalførtHendelsesType() {
-        // TODO
+        JournalpostEndretEvent journalpostEndretEvent = createJournalpostEndretEvent("DAG", "FOR", "M", "J", "D", "I");
+        InngaaendeHendelse map = publisher.map(journalpostEndretEvent);
+        assertEquals(ENDELIG_JOURNALFORT, map.getHendelsesType());
     }
 
     @Test
-    @Ignore
     public void shouldFinnJournalfpostUgåttHendelsesType() {
-        // TODO
+        JournalpostEndretEvent journalpostEndretEvent = createJournalpostEndretEvent("", "", "MO", "U", "U", "I");
+        InngaaendeHendelse map = publisher.map(journalpostEndretEvent);
+        assertEquals(JOURNALPOST_UTGATT, map.getHendelsesType());
     }
 
     @Test
-    @Ignore
     public void shouldNotMapToHendelsesType() {
-        // TODO
+        JournalpostEndretEvent journalpostEndretEvent = createJournalpostEndretEvent("", "", "", "", "", "");
+        assertNull(publisher.map(journalpostEndretEvent));
     }
 
 }
