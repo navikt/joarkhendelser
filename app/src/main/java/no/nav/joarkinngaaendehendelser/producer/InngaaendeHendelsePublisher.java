5dfb5b8605d7c278ee07f7216a9292a23359024a
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2018-10-04 17:25:02 +0200

    MMA-784: Endre InngåendeHendelsesType til String

--- app/src/main/java/no/nav/joarkinngaaendehendelser/producer/InngaaendeHendelsePublisher.java
+++ app/src/main/java/no/nav/joarkinngaaendehendelser/producer/InngaaendeHendelsePublisher.java
@@ -1,5 +1,6 @@
 package no.nav.joarkinngaaendehendelser.producer;
 
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutionException;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +18,20 @@ import lombok.extern.slf4j.Slf4j;
 public class InngaaendeHendelsePublisher {
 
     @Autowired
-    private KafkaTemplate<Object, Object> kafkaTemplate;
+    private KafkaTemplate<String, InngaaendeHendelse> kafkaTemplate;
 
     @Value("${inngaaendeJournalpostEndret.topic}")
     private String topic;
 
     public void publish(InngaaendeHendelse hendelse) {
-        ProducerRecord<Object, Object> producerRecord = new ProducerRecord<>(
+        ProducerRecord<String, InngaaendeHendelse> producerRecord = new ProducerRecord<>(
                 topic,
                 null,
                 hendelse.getTimestamp(),
                 hendelse.getJournalpostId(),
                 hendelse);
+        producerRecord.headers().add("hendelsesId", hendelse.getJournalpostId().getBytes(StandardCharsets.UTF_8));
+        producerRecord.headers().add("versjon", hendelse.getVersjon().getBytes(StandardCharsets.UTF_8));
         try {
             kafkaTemplate.send(producerRecord).get();
         }
