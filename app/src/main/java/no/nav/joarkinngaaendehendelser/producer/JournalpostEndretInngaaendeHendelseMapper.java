71f87cd88d26721704db13cd645edf5acbbd8d5a
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2018-10-08 15:47:07 +0200

    MMA-626 Splittet hendelsemapper ut i flere hjelpefunksjoner

--- app/src/main/java/no/nav/joarkinngaaendehendelser/producer/JournalpostEndretInngaaendeHendelseMapper.java
+++ app/src/main/java/no/nav/joarkinngaaendehendelser/producer/JournalpostEndretInngaaendeHendelseMapper.java
@@ -9,10 +9,10 @@ import static no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostStatus.U
 import static no.nav.joarkinngaaendehendelser.consumer.kafka.OracleSchema.INSERT_OPERATION;
 import static no.nav.joarkinngaaendehendelser.consumer.kafka.OracleSchema.UPDATE_OPERATION;
 
-import java.util.UUID;
+import no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostEndretEvent;
 import org.apache.commons.lang.StringUtils;
 
-import no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostEndretEvent;
+import java.util.UUID;
 
 /**
  * @author Martin Burheim Tingstad, Visma Consulting.
@@ -37,29 +37,79 @@ public class JournalpostEndretInngaaendeHendelseMapper {
     }
 
     private static InngaaendeHendelsesType finnHendelsesType(JournalpostEndretEvent event) {
-        if(INSERT_OPERATION.equalsIgnoreCase(event.getOperation()) &&
-                INNGAAENDE.equalsIgnoreCase(event.getJournalpostType()) &&
-                (MOTTATT.equalsIgnoreCase(event.getJournalpostStatusAfter()) || MIDLERTIDIG.equalsIgnoreCase(event.getJournalpostStatusAfter()))) {
-            return InngaaendeHendelsesType.MIDLERTIDIG_JOURNALFORT;
+        InngaaendeHendelsesType hendelsesType = null;
+
+        if (!isInngaaende(event)) {
+            hendelsesType = null;
+        } else if (isMidlertidigJournalfort(event)) {
+            hendelsesType = InngaaendeHendelsesType.MIDLERTIDIG_JOURNALFORT;
+        } else if (isTemaEndret(event)) {
+            hendelsesType = InngaaendeHendelsesType.TEMA_ENDRET;
+        } else if (isEndeligJournalfort(event)) {
+            hendelsesType = InngaaendeHendelsesType.ENDELIG_JOURNALFORT;
+        } else if (isJournalpostUtgatt(event)) {
+            hendelsesType = InngaaendeHendelsesType.JOURNALPOST_UTGATT;
         }
-        if(UPDATE_OPERATION.equalsIgnoreCase(event.getOperation()) &&
-                INNGAAENDE.equalsIgnoreCase(event.getJournalpostType()) &&
-                StringUtils.isNotEmpty(event.getFagomradeBefore()) &&
+
+        return hendelsesType;
+    }
+
+    private static boolean isMidlertidigJournalfort(JournalpostEndretEvent event) {
+        return isInsertOperation(event) &&
+                (isMottatt(event) || isMidlertidig(event));
+    }
+
+    private static boolean isTemaEndret(JournalpostEndretEvent event) {
+        return isUpdateOperation(event) &&
+                hasChangedFagomrade(event);
+    }
+
+    private static boolean isEndeligJournalfort(JournalpostEndretEvent event) {
+        return wasMidlertidig(event) &&
+                isJournalfort(event);
+    }
+
+    private static boolean isJournalpostUtgatt(JournalpostEndretEvent event) {
+        return isUpdateOperation(event) &&
+                hasChangedJournalpostStatusToUtgarOrUkjentbruker(event);
+    }
+
+    private static boolean hasChangedFagomrade(JournalpostEndretEvent event) {
+        return StringUtils.isNotEmpty(event.getFagomradeBefore()) &&
                 StringUtils.isNotEmpty(event.getFagomradeAfter()) &&
-                !event.getFagomradeBefore().equalsIgnoreCase(event.getFagomradeAfter())) {
-            return InngaaendeHendelsesType.TEMA_ENDRET;
-        }
-        if(INNGAAENDE.equalsIgnoreCase(event.getJournalpostType()) &&
-                MIDLERTIDIG.equalsIgnoreCase(event.getJournalpostStatusBefore()) &&
-                JOURNALFORT.equalsIgnoreCase(event.getJournalpostStatusAfter())) {
-            return InngaaendeHendelsesType.ENDELIG_JOURNALFORT;
-        }
-        if(UPDATE_OPERATION.equalsIgnoreCase(event.getOperation()) &&
-                INNGAAENDE.equalsIgnoreCase(event.getJournalpostType()) &&
-                StringUtils.isNotEmpty(event.getJournalpostStatusBefore()) &&
-                (UTGAR.equalsIgnoreCase(event.getJournalpostStatusAfter()) || UKJENTBRUKER.equalsIgnoreCase(event.getJournalpostStatusAfter()))) {
-            return InngaaendeHendelsesType.JOURNALPOST_UTGATT;
-        }
-        return null;
+                !event.getFagomradeBefore().equalsIgnoreCase(event.getFagomradeAfter());
+    }
+
+    private static boolean hasChangedJournalpostStatusToUtgarOrUkjentbruker(JournalpostEndretEvent event) {
+        return StringUtils.isNotEmpty(event.getJournalpostStatusBefore()) &&
+                (UTGAR.equalsIgnoreCase(event.getJournalpostStatusAfter()) || UKJENTBRUKER.equalsIgnoreCase(event.getJournalpostStatusAfter()));
+    }
+
+    private static boolean isInsertOperation(JournalpostEndretEvent event) {
+        return INSERT_OPERATION.equalsIgnoreCase(event.getOperation());
+    }
+
+    private static boolean isUpdateOperation(JournalpostEndretEvent event) {
+        return UPDATE_OPERATION.equalsIgnoreCase(event.getOperation());
+    }
+
+    private static boolean isInngaaende(JournalpostEndretEvent event) {
+        return INNGAAENDE.equalsIgnoreCase(event.getJournalpostType());
+    }
+
+    private static boolean isJournalfort(JournalpostEndretEvent event) {
+        return JOURNALFORT.equalsIgnoreCase(event.getJournalpostStatusAfter());
+    }
+
+    private static boolean isMottatt(JournalpostEndretEvent event) {
+        return MOTTATT.equalsIgnoreCase(event.getJournalpostStatusAfter());
+    }
+
+    private static boolean isMidlertidig(JournalpostEndretEvent event) {
+        return MIDLERTIDIG.equalsIgnoreCase(event.getJournalpostStatusAfter());
+    }
+
+    private static boolean wasMidlertidig(JournalpostEndretEvent event) {
+        return MIDLERTIDIG.equalsIgnoreCase(event.getJournalpostStatusBefore());
     }
 }
