36adc0b1a3db8684a0d5b3b7cd9d0742e4b224ec
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2018-10-17 17:53:10 +0200

    MMA-626: litt test-cleanup

--- app/src/test/java/no/nav/joarkinngaaendehendelser/consumer/kafka/JournalpostEndretListenerTest.java
+++ app/src/test/java/no/nav/joarkinngaaendehendelser/consumer/kafka/JournalpostEndretListenerTest.java
@@ -1,74 +1,87 @@
 package no.nav.joarkinngaaendehendelser.consumer.kafka;
 
-import static no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostStatus.INNGAAENDE;
 import static no.nav.joarkinngaaendehendelser.consumer.kafka.JoarkSchema.JOURNALPOST;
+import static no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostStatus.INNGAAENDE;
+import static no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostStatus.UTGAR;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.HashSet;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelse;
+import no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelsePublisher;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.slf4j.Logger;
 
-import no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelse;
-import no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelsePublisher;
+import java.util.HashSet;
 
 @RunWith(MockitoJUnitRunner.class)
 public class JournalpostEndretListenerTest {
 
-    @Mock
-    private ConsumerRecord<?, byte[]> consumerRecordMock;
+	@Mock
+	private ConsumerRecord<?, byte[]> consumerRecordMock;
+	@Mock
+	private ConsumerRecordAsJsonConverter converterMock;
+	@Mock
+	private InngaaendeHendelsePublisher publisher;
+	@Mock
+	private MeterRegistry meterRegistry;
+	@Mock
+	private Counter counterMock;
+	@InjectMocks
+	private JournalpostEndretListener listener;
 
-    @Mock
-    private ConsumerRecordAsJsonConverter converterMock;
+	private JournalpostEndretEvent event;
 
-    @Mock
-    private InngaaendeHendelsePublisher publisher;
+	@Before
+	public void before() {
+		when(meterRegistry.counter(anyString(), anyString(), anyString())).thenReturn(counterMock);
+	}
 
-    @Mock
-    Logger log;
+	@Test
+	public void shouldPublishEvent() {
+		event = createJournalpostEndretEvent(INNGAAENDE);
+		when(converterMock.convertRecordToEvent(any(ConsumerRecord.class))).thenReturn(event);
 
-    @InjectMocks
-    private JournalpostEndretListener listener;
+		listener.onMessage(consumerRecordMock);
 
-    @Before
-    public void before() throws Exception {
-        HashSet<String> columnsChanged = new HashSet<>();
-        columnsChanged.add(JOURNALPOST);
+		verify(converterMock).convertRecordToEvent(consumerRecordMock);
+		verify(publisher).publish(any(InngaaendeHendelse.class));
+		verify(meterRegistry, times(1)).counter(anyString(), anyString(), anyString());
+		verify(counterMock, times(1)).increment();
+	}
 
-        when(converterMock.convertRecordToEvent(any(ConsumerRecord.class))).thenReturn(
-                JournalpostEndretEvent.builder()
-                        .journalpostId(123L)
-                        .fagomradeBefore("FOR")
-                        .fagomradeAfter("BAR")
-                        .journalpostStatusAfter("M")
-                        .journalpostType(INNGAAENDE)
-                        .operation("U")
-                        .columnsChanged(columnsChanged)
-                        .build());
-    }
+	@Test
+	public void shouldNotPublishAnyEventIfJournalpostTypeIsNotInngaaende() {
+		event = createJournalpostEndretEvent(UTGAR);
+		when(converterMock.convertRecordToEvent(any(ConsumerRecord.class))).thenReturn(event);
 
-    @Test
-    public void onUpdateMessage() throws Exception {
-        listener.onMessage(consumerRecordMock);
-        verify(converterMock).convertRecordToEvent(consumerRecordMock);
-    }
+		listener.onMessage(consumerRecordMock);
 
-    @Ignore
-    @Test
-    public void onCreatedMessage() throws Exception {
-        listener.onMessage(consumerRecordMock);
-        verify(converterMock).convertRecordToEvent(consumerRecordMock);
-        verify(publisher).publish(any(InngaaendeHendelse.class));
-        verify(log, times(1)).info(any());
-    }
+		verify(converterMock, times(1)).convertRecordToEvent(consumerRecordMock);
+		verify(publisher, times(0)).publish(any(InngaaendeHendelse.class));
+	}
 
+	private JournalpostEndretEvent createJournalpostEndretEvent(String journalpostType) {
+		HashSet<String> columnsChanged = new HashSet<>();
+		columnsChanged.add(JOURNALPOST);
+
+		return JournalpostEndretEvent.builder()
+				.journalpostId(123L)
+				.fagomradeBefore("FOR")
+				.fagomradeAfter("BAR")
+				.journalpostStatusAfter("M")
+				.journalpostType(journalpostType)
+				.operation("U")
+				.columnsChanged(columnsChanged)
+				.build();
+	}
 }
--- app/src/test/java/no/nav/joarkinngaaendehendelser/producer/InngaaendeHendelsePublisherTest.java
+++ app/src/test/java/no/nav/joarkinngaaendehendelser/producer/InngaaendeHendelsePublisherTest.java
@@ -1,49 +1,64 @@
 package no.nav.joarkinngaaendehendelser.producer;
 
+import static no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelsesType.MIDLERTIDIG_JOURNALFORT;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostEndretEvent;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.concurrent.ListenableFuture;
 
+@RunWith(MockitoJUnitRunner.class)
 public class InngaaendeHendelsePublisherTest {
 
-    JournalpostEndretEvent journalpostEndretEvent;
+	@InjectMocks
+	InngaaendeHendelsePublisher inngaaendeHendelsePublisher;
 
-    @Mock
-    ProducerRecord<String, InngaaendeHendelseRecord> producerRecord;
+	@Mock
+	private KafkaTemplate<String, InngaaendeHendelseRecord> kafkaTemplate;
 
-    @Autowired
-    InngaaendeHendelsePublisher publisher;
+	private InngaaendeHendelse hendelse;
 
-    @Mock
-    private KafkaTemplate<String, InngaaendeHendelseRecord> kafkaTemplate;
+	@Before
+	public void setUp() throws Exception {
+		ReflectionTestUtils.setField(inngaaendeHendelsePublisher, "topic", "test-topic");
 
-    @Before
-    public void setUp() throws Exception {
-        journalpostEndretEvent = JournalpostEndretEvent.builder()
-                .journalpostStatusAfter("MO")
-                .journalpostStatusBefore("MO")
-                .operation("I")
-                .journalpostType("I")
-                .build();
-    }
+		ListenableFuture listenableFuture = mock(ListenableFuture.class);
+		when(kafkaTemplate.send(any(ProducerRecord.class))).thenReturn(listenableFuture);
+		when(listenableFuture.get()).thenReturn("ok");
+	}
 
-    @After
-    public void tearDown() throws Exception {
-    }
+	@Test
+	public void shouldPublish() {
+		hendelse = createInngaaendeHendelse();
+		inngaaendeHendelsePublisher.publish(hendelse);
 
-    @Test
-    @Ignore
-    public void shouldPublish() throws Exception {
-        InngaaendeHendelse map = JournalpostEndretInngaaendeHendelseMapper.map(journalpostEndretEvent);
-        publisher.publish(map);
+		verify(kafkaTemplate, times(1)).send(any(ProducerRecord.class));
+		assertEquals(MIDLERTIDIG_JOURNALFORT.toString(), hendelse.getHendelsesType());
+	}
 
-//        verify(kafkaTemplate).send(producerRecord);
-        //assertEquals(MIDLERTIDIG_JOURNALFORT, map.getHendelsesType());
-    }
+	private InngaaendeHendelse createInngaaendeHendelse() {
+		JournalpostEndretEvent journalpostEndretEvent = JournalpostEndretEvent.builder()
+				.journalpostId(1L)
+				.journalpostStatusAfter("MO")
+				.journalpostStatusBefore("MO")
+				.operation("I")
+				.journalpostType("I")
+				.kanalReferanseId("kanal-ref")
+				.build();
+
+		return JournalpostEndretInngaaendeHendelseMapper.map(journalpostEndretEvent);
+	}
 }
