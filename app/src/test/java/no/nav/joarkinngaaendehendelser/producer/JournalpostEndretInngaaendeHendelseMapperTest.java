f3c935884867e6886603f1a6f7351c03356e9eb7
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2018-10-17 13:29:47 +0200

    MMA-784 Økt coverage i mapper-test.

--- app/src/test/java/no/nav/joarkinngaaendehendelser/producer/JournalpostEndretInngaaendeHendelseMapperTest.java
+++ app/src/test/java/no/nav/joarkinngaaendehendelser/producer/JournalpostEndretInngaaendeHendelseMapperTest.java
@@ -1,11 +1,5 @@
 package no.nav.joarkinngaaendehendelser.producer;
 
-import no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostEndretEvent;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.kafka.core.KafkaTemplate;
-
 import static no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelsesType.ENDELIG_JOURNALFORT;
 import static no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelsesType.JOURNALPOST_UTGATT;
 import static no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelsesType.MIDLERTIDIG_JOURNALFORT;
@@ -13,14 +7,20 @@ import static no.nav.joarkinngaaendehendelser.producer.InngaaendeHendelsesType.T
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import no.nav.joarkinngaaendehendelser.consumer.kafka.JournalpostEndretEvent;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.kafka.core.KafkaTemplate;
+
 public class JournalpostEndretInngaaendeHendelseMapperTest {
 
     private JournalpostEndretEvent createJournalpostEndretEvent(String fagomradeBefore, String fagomradeAfter, String journalpostStatusBefore, String journalpostStatusAfter, String operation, String journalposttype) {
         return JournalpostEndretEvent.builder()
                 .fagomradeBefore(fagomradeBefore)
                 .fagomradeAfter(fagomradeAfter)
-                .journalpostStatusAfter(journalpostStatusAfter)
                 .journalpostStatusBefore(journalpostStatusBefore)
+                .journalpostStatusAfter(journalpostStatusAfter)
                 .operation(operation)
                 .journalpostType(journalposttype)
                 .build();
@@ -34,9 +34,12 @@ public class JournalpostEndretInngaaendeHendelseMapperTest {
 
     @Test
     public void shouldFinnMidlertidigJournalførtHendelsesType() {
-        JournalpostEndretEvent journalpostEndretEvent = createJournalpostEndretEvent("DAG", "FOR", "MO", "MO", "I", "I");
-        InngaaendeHendelse map = mapper.map(journalpostEndretEvent);
-        assertEquals(MIDLERTIDIG_JOURNALFORT.toString(), map.getHendelsesType());
+        JournalpostEndretEvent journalpostEndretEventMidlertidig = createJournalpostEndretEvent("DAG", "FOR", "M", "M", "I", "I");
+        InngaaendeHendelse mapMidlertidig = mapper.map(journalpostEndretEventMidlertidig);
+        JournalpostEndretEvent journalpostEndretEventMottatt = createJournalpostEndretEvent("DAG", "FOR", "MO", "MO", "I", "I");
+        InngaaendeHendelse mapMottatt = mapper.map(journalpostEndretEventMottatt);
+        assertEquals(MIDLERTIDIG_JOURNALFORT.toString(), mapMottatt.getHendelsesType());
+        assertEquals(MIDLERTIDIG_JOURNALFORT.toString(), mapMidlertidig.getHendelsesType());
     }
 
     @Test
@@ -55,9 +58,12 @@ public class JournalpostEndretInngaaendeHendelseMapperTest {
 
     @Test
     public void shouldFinnJournalfpostUgåttHendelsesType() {
-        JournalpostEndretEvent journalpostEndretEvent = createJournalpostEndretEvent("", "", "MO", "U", "U", "I");
-        InngaaendeHendelse map = mapper.map(journalpostEndretEvent);
-        assertEquals(JOURNALPOST_UTGATT.toString() , map.getHendelsesType());
+        JournalpostEndretEvent journalpostEndretEventUtgatt = createJournalpostEndretEvent("", "", "MO", "U", "U", "I");
+        JournalpostEndretEvent journalpostEndretEventUkjent = createJournalpostEndretEvent("", "", "MO", "UB", "U", "I");
+        InngaaendeHendelse mapUtgatt = mapper.map(journalpostEndretEventUtgatt);
+        InngaaendeHendelse mapUkjent = mapper.map(journalpostEndretEventUkjent);
+        assertEquals(JOURNALPOST_UTGATT.toString(), mapUtgatt.getHendelsesType());
+        assertEquals(JOURNALPOST_UTGATT.toString(), mapUkjent.getHendelsesType());
     }
 
     @Test
