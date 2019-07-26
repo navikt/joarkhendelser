3828511772600d00c9d00aad29be07fa94377461
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2019-07-26 09:14:36 +0200

    MMA-3168 La til metrikk på Nais isReady og fiks på feilhåndtering

--- app/src/main/java/no/nav/joarkjournalfoeringhendelser/config/KafkaErrorHandler.java
+++ app/src/main/java/no/nav/joarkjournalfoeringhendelser/config/KafkaErrorHandler.java
@@ -1,8 +1,11 @@
 package no.nav.joarkjournalfoeringhendelser.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.errors.AuthenticationException;
+import org.apache.kafka.common.errors.AuthorizationException;
 import org.springframework.kafka.listener.ContainerAwareErrorHandler;
 import org.springframework.kafka.listener.ContainerStoppingErrorHandler;
 import org.springframework.kafka.listener.MessageListenerContainer;
@@ -21,32 +24,37 @@ public class KafkaErrorHandler implements ContainerAwareErrorHandler {
 
 	private static final ContainerStoppingErrorHandler STOPPING_ERROR_HANDLER = new ContainerStoppingErrorHandler();
 
-	private AtomicInteger counter = new AtomicInteger(0);
+	public static final AtomicInteger authorizationErrorCounter = new AtomicInteger(0);
+	private final MeterRegistry meterRegistry;
+
+	public KafkaErrorHandler(MeterRegistry meterRegistry) {
+		this.meterRegistry = meterRegistry;
+	}
 
 	@Override
 	public void handle(Exception e, List<ConsumerRecord<?, ?>> records, Consumer<?, ?> consumer, MessageListenerContainer container) {
-		records.stream()
-				.map(ConsumerRecord::topic)
-				.findAny()
-				.ifPresent(topic -> scheduleRestart(e, records, consumer, container, topic));
+		log.warn("KafkaContainer feilet med feilmelding={}", e.getMessage(), e);
+		meterRegistry.counter("dok_exception", "type", "technical");
+		if (e instanceof AuthenticationException || e instanceof AuthorizationException) {
+			log.warn("Forsøker å restarte Kafka container for {}", String.join(", ", container.getContainerProperties().getTopics()));
+			authorizationErrorCounter.incrementAndGet();
+			scheduleRestart(e, records, consumer, container);
+		}
 	}
 
 	@SuppressWarnings({"pmd:DoNotUseThreads", "fb-contrib:SEC_SIDE_EFFECT_CONSTRUCTOR"})
-	private void scheduleRestart(Exception e, List<ConsumerRecord<?, ?>> records, Consumer<?, ?> consumer, MessageListenerContainer container, String topic) {
+	private void scheduleRestart(Exception e, List<ConsumerRecord<?, ?>> records, Consumer<?, ?> consumer, MessageListenerContainer container) {
 		new Thread(() -> {
 			try {
-				long slow = Duration.ofHours(3).toMillis();
-				long fast = Duration.ofSeconds(20).toMillis();
-				Thread.sleep((counter.get() > 10) ? slow : fast * counter.getAndIncrement());
-				log.warn("Starter kafka container for {}", topic);
+				Thread.sleep(Duration.ofSeconds(10).toMillis());
+				log.warn("Starter kafka container for {}", String.join(", ", container.getContainerProperties().getTopics()));
 				container.start();
-				counter.set(0);
 			} catch (Exception exception) {
 				log.error("Feil oppstod ved venting og oppstart av kafka container", exception);
 			}
 		}).start();
 
-		log.warn("Stopper kafka container for {}", topic);
+		log.warn("Stopper kafka container for {}", String.join(", ", container.getContainerProperties().getTopics()));
 		STOPPING_ERROR_HANDLER.handle(e, records, consumer, container);
 	}
 }
--- app/src/main/java/no/nav/joarkjournalfoeringhendelser/nais/NaisContract.java
+++ app/src/main/java/no/nav/joarkjournalfoeringhendelser/nais/NaisContract.java
@@ -1,5 +1,9 @@
 package no.nav.joarkjournalfoeringhendelser.nais;
 
+import static no.nav.joarkjournalfoeringhendelser.config.KafkaErrorHandler.authorizationErrorCounter;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Slf4j
 @RestController
 public class NaisContract {
@@ -15,10 +21,18 @@ public class NaisContract {
 	public static final String APPLICATION_ALIVE = "Application is alive!";
 	public static final String APPLICATION_READY = "Application is ready for traffic!";
 	private static final String APPLICATION_NOT_READY = "Application is not ready for traffic :-(";
+	private static AtomicInteger isReady = new AtomicInteger(1);
+
+	public NaisContract(MeterRegistry meterRegistry) {
+		Gauge.builder("dok_app_is_ready", isReady, AtomicInteger::get).register(meterRegistry);
+	}
 
 	@GetMapping("/isAlive")
-	public String isAlive() {
-		return APPLICATION_ALIVE;
+	public ResponseEntity isAlive() {
+		if (authorizationErrorCounter.get()>5){
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		return ResponseEntity.ok(APPLICATION_ALIVE);
 	}
 
 	@RequestMapping(value = "/isReady", produces = MediaType.TEXT_PLAIN_VALUE)
