a246f92523c3164b0e46dc37598e74c514d3120b
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2019-07-31 11:59:35 +0200

    MMA-3168 Fjernet filer som ikke brukes

--- app/src/main/java/no/nav/joarkjournalfoeringhendelser/Application.java
+++ app/src/main/java/no/nav/joarkjournalfoeringhendelser/Application.java
@@ -2,7 +2,6 @@ package no.nav.joarkjournalfoeringhendelser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 
 @Import(value = {
@@ -12,8 +11,6 @@ import org.springframework.context.annotation.Import;
 public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext run =
-				SpringApplication.run(Application.class, args);
-		run.addApplicationListener(new ApplicationErrorListener());
+		SpringApplication.run(Application.class, args);
 	}
 }
