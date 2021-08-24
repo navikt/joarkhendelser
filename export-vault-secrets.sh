2d2b4ca080d182f6a2d8c52ecf90140cb858d3b5
A157857 <tim.aasen@nav.no>
2021-08-24 13:03:13 +0200

    Endringer applikasjonen for å endre navn

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-if test -f /var/run/secrets/nais.io/joarkhendelser/username;
+if test -f /var/run/secrets/nais.io/srvjoarkhendelser/username;
 then
     echo "Setting SERVICEUSER_USERNAME"
-    export SERVICEUSER_USERNAME=$(cat /var/run/secrets/nais.io/joarkhendelser/username)
+    export SERVICEUSER_USERNAME=$(cat /var/run/secrets/nais.io/srvjoarkhendelser/username)
 fi
 
-if test -f /var/run/secrets/nais.io/joarkhendelser/password;
+if test -f /var/run/secrets/nais.io/srvjoarkhendelser/password;
 then
     echo "Setting SERVICEUSER_PASSWORD"
-    export SERVICEUSER_PASSWORD=$(cat /var/run/secrets/nais.io/joarkhendelser/password)
+    export SERVICEUSER_PASSWORD=$(cat /var/run/secrets/nais.io/srvjoarkhendelser/password)
 fi
