dfc2224aa48ed788ab64e17065eb7ee4f67b0b5f
Anders Opskar Voldsund <anders.voldsund@nav.no>
2021-12-16 17:57:30 +0100

    Rydd opp i exceptions

---
+++ app/src/main/java/no/nav/joarkhendelser/exception/AuthenticationFailedException.java
@@ -0,0 +1,7 @@
+package no.nav.joarkhendelser.exception;
+
+public class AuthenticationFailedException extends JoarkhendelserTechnicalException {
+	public AuthenticationFailedException(String s, Throwable t) {
+		super(s, t);
+	}
+}
